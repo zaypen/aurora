1b678c663cfece3649a0e869d10755dc23577761
zaypen <zaypen@gmail.com>
2020-12-15 21:51:57 +0800

    Tidy Launchpad

--- lib/devices/Launchpad.dart
+++ lib/devices/Launchpad.dart
@@ -5,6 +5,9 @@ import 'package:aurora/devices/Pos.dart';
 import 'package:flutter_midi_command/flutter_midi_command.dart';
 
 class Launchpad {
+  static const SYS_EX_START = 240;
+  static const SYS_EX_END = 247;
+  static const SYS_EX_HEADER = [0, 32, 41, 2];
   final MidiCommand _midiCommand = MidiCommand();
 
   _write(List<int> bytes) {
@@ -12,15 +15,19 @@ class Launchpad {
   }
 
   _writeSysEx(List<int> bytes) {
-    _write([0xf0] + bytes + [0xf7]);
+    _write([SYS_EX_START] + bytes + [SYS_EX_END]);
+  }
+
+  setLayout(int layout, int page) {
+    _writeSysEx(SYS_EX_HEADER + [14, 0, layout.clamp(0, 19), page, 0]);
   }
 
   setProgrammerMode(int mode) {
-    _writeSysEx([0, 32, 41, 2, 14, 14, mode.clamp(0, 1)]);
+    _writeSysEx(SYS_EX_HEADER + [14, 14, mode.clamp(0, 1)]);
   }
 
   setCellRgb(Pos pos, Color color) {
-    _writeSysEx([0, 32, 41, 2, 14, 3, 3, pos.number] + color.bytes());
+    _writeSysEx(SYS_EX_HEADER + [14, 3, 3, pos.number] + color.bytes());
   }
 
   setCellPalette(Pos pos, int color) {
